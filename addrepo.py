619b6ffc9eeccfac67b1d78585f801d10f36aeba
T-051 <yamt015@hotmail.com>
2019-02-12 11:30:20 +0800

    Update addrepo.py

--- addrepo.py
+++ addrepo.py
@@ -5,9 +5,9 @@
 import json
 
 proj_data = {
-	'name': input('Name of the repo: '),
-	'link': input('Link of the repo: '),
-	'page': input('Page of the project(blank if don\'t have): ')
+    'name': input('Name of the repo: '),
+    'link': input('Link of the repo: '),
+    'page': input('Page of the project(blank if don\'t have): ')
 }
 template = "## List\n* [{}]({})".format(proj_data['name'], proj_data['link'])
 
@@ -16,7 +16,7 @@ data = json.load(data_raw)
 data_raw.close()
 data.append(proj_data)
 data_raw = open('data/repo.json', 'w')
-data_raw.write(json.dumps(data, sort_keys=True, indent=4))
+data_raw.write(json.dumps(data, sort_keys=True, indent=2))
 data_raw.close()
 
 fr = open('docs/README.md').read()
